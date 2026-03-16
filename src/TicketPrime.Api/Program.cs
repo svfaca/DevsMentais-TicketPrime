ddeb8fbd2734616b080e0d2ba4ad74883e389137
Joao Vitor <luhdens@gmail.com>
2026-03-16 12:20:43 -0300

    finalização endpoints API AV1

--- src/TicketPrime.Api/Program.cs
+++ src/TicketPrime.Api/Program.cs
@@ -5,9 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 string connectionString =
-    "Server=(localdb)\\MSSQLLocalDB;Database=TicketPrime;Trusted_Connection=True;TrustServerCertificate=True;";
-
-app.MapGet("/", () => "TicketPrime API online");
+"Server=(localdb)\\MSSQLLocalDB;Database=TicketPrime;Trusted_Connection=True;TrustServerCertificate=True;";
 
 app.MapGet("/api/eventos", async () =>
 {
@@ -16,6 +14,71 @@ app.MapGet("/api/eventos", async () =>
     var eventos = await connection.QueryAsync("SELECT * FROM Eventos");
 
     return Results.Ok(eventos);
+});
+
+app.MapPost("/api/eventos", async (string nome, int capacidadeTotal, DateTime dataEvento, decimal precoPadrao) =>
+{
+    using var connection = new SqlConnection(connectionString);
+
+    var sql = @"
+        INSERT INTO Eventos (Nome, CapacidadeTotal, DataEvento, PrecoPadrao)
+        VALUES (@nome, @capacidadeTotal, @dataEvento, @precoPadrao)";
+
+    await connection.ExecuteAsync(sql, new
+    {
+        nome,
+        capacidadeTotal,
+        dataEvento,
+        precoPadrao
+    });
+
+    return Results.Created();
+});
+
+app.MapPost("/api/cupons", async (string codigo, decimal porcentagemDesconto, decimal valorMinimoRegra) =>
+{
+    using var connection = new SqlConnection(connectionString);
+
+    var sql = @"
+        INSERT INTO Cupons (Codigo, PorcentagemDesconto, ValorMinimoRegra)
+        VALUES (@codigo, @porcentagemDesconto, @valorMinimoRegra)";
+
+    await connection.ExecuteAsync(sql, new
+    {
+        codigo,
+        porcentagemDesconto,
+        valorMinimoRegra
+    });
+
+    return Results.Created();
+});
+
+app.MapPost("/api/usuarios", async (string cpf, string nome, string email) =>
+{
+    using var connection = new SqlConnection(connectionString);
+
+    var usuarioExistente = await connection.QueryFirstOrDefaultAsync(
+        "SELECT * FROM Usuarios WHERE Cpf = @cpf",
+        new { cpf }
+    );
+
+    if (usuarioExistente != null)
+    {
+        return Results.BadRequest("CPF já cadastrado");
+    }
+
+    var sql = @"
+        INSERT INTO Usuarios (Cpf, Nome, Email)
+        VALUES (@cpf, @nome, @email)";
+
+    await connection.ExecuteAsync(sql, new
+    {
+        cpf,
+        nome,
+        email
+    });
+
+    return Results.Created();
 });
 
 app.Run();
